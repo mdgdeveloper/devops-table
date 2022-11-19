ee75d13c9db37da577bfb93fa663afaede4d9539
mdgdeveloper <se.rgiomadrigal@gmail.com>
2022-11-19 18:19:12 +0100

    update: footer structure

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,7 +3,14 @@ import "./footer.css";
 const Footer = () => {
   return (
     <div className="f-global">
-      <div>(c) 2022 - MDGdev</div>
+      <div className="f-flex">
+        <div className="f-left"></div>
+        <div className="f-center"></div>
+        <div className="f-right"></div>
+      </div>
+      <div className="f-footer">
+        <div className="f-copy"></div>
+      </div>
     </div>
   );
 };
