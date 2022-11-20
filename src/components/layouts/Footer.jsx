42980e8986f3decbc774f3a0eb6e58fd968c603f
mdgdeveloper <se.rgiomadrigal@gmail.com>
2022-11-21 00:01:00 +0100

    structure: footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,10 +3,32 @@ import "./footer.css";
 const Footer = () => {
   return (
     <div className="f-global">
-      <div className="f-flex">
-        <div className="f-left"></div>
-        <div className="f-center"></div>
-        <div className="f-right"></div>
+      <div className="f-double">
+        <div className="f-flex">
+          <div className="f-left">
+            <div className="f-logo">logo</div>
+            <div className="f-logo-text">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Molestiae, nisi amet qui distinctio aut totam suscipit
+              repudiandae, esse voluptatibus praesentium accusantium id a! Nam,
+              corporis laboriosam magni iure quisquam cumque?
+            </div>
+          </div>
+          <div className="f-center">
+            <div className="f-title">Contact Us</div>
+            <div className="f-center-mail">contact@mdgdev.xyz</div>
+            <div className="f-center-info">Valencia. Spain. Europe.</div>
+          </div>
+          <div className="f-right">
+            <div className="f-title">Follow Us</div>
+            <div className="f-right-text">Yes, you can follow us!</div>
+            <div className="f-right-social">
+              <div className="f-social-item">Twitter</div>
+              <div className="f-social-item">Mastodon</div>
+              <div className="f-social-item">Hashnode</div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="f-footer">
         <div className="f-copy"></div>
